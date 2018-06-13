fdd88605816255cbe3b894b289dc704e79098eab
Sk Saddam Hosan <sksddmhosan@gmail.com>
2018-06-13 11:45:21 +0530

    some changes in JsonView ...successfully done

--- restapi/src/main/java/com/backend/restapi/common/JsonViewModel.java
+++ restapi/src/main/java/com/backend/restapi/common/JsonViewModel.java
@@ -15,7 +15,42 @@ package com.backend.restapi.common;
 public class JsonViewModel {
 	
 	public interface SuccResponseModel{}
+	
+	// This is for the login  
 	public  interface LoginView extends SuccResponseModel{}
+	
+	// This is for the OrderController
+	
+	public interface OrderView extends SuccResponseModel{}
+	
+	
+	// This is for the ProductController
+	
+	public interface ProductView extends SuccResponseModel{}
+	
+	// This is for UserController
+	
+	public interface UserView extends SuccResponseModel{}
+	
+	// This is for UserAddView in UserContrller
+	
+	public interface UserAddView extends SuccResponseModel{}
+	
+	// UpdateView in UserController
+	
+	public interface UpdateUserView extends SuccResponseModel{}
+	
+	// This is for ShpKeeper Add view in UserController
+	
+	public interface ShopKeeperAddView extends SuccResponseModel{}
+	
+	// This is for update shopkeeper view in UserController
+	
+	public interface updateShopkeereView extends SuccResponseModel{}
+	
+
+	
+	
 	
 	
 
